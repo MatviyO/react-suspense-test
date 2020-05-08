656bf57de39e0cf6590d1f162851b4536963f4d1
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-05-08 14:34:27 +0300

    up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import {useResource} from "./resource";
 import {Posts} from "./Posts";
+import {Users} from "./Users";
 
 const resource = useResource()
 
@@ -8,7 +9,12 @@ function App() {
   return (
     <div className="container">
         <h1>suspense</h1>
-        <Posts resource={resource} />
+        <Suspense fallback={<p>Loading...</p>}>
+            <Posts resource={resource} />
+        </Suspense>
+        <Suspense fallback={<p>Loading users...</p>}>
+            <Users resource={resource} />
+        </Suspense>
     </div>
   );
 }
--- src/resource.js
+++ src/resource.js
@@ -1,26 +1,39 @@
 export function useResource() {
     return {
-        posts: wrapPromise(fetchPosts())
+        posts: wrapPromise(fetchPosts()),
+        users: wrapPromise(fetchUsers())
     }
 }
+
+const delay = ms => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve()
+        }, ms)
+    })
+}
+
 function wrapPromise(promise) {
     let status = 'pending'
     let result
-    const suspender = promise
-        .then(r => {
+    const suspender = promise.then(
+        r => {
             result = r
             status = 'success'
-        }, e => {
+        },
+        e => {
             result = e
             status = 'error'
-        })
+        }
+    )
+
     return {
         read() {
-            if ( status === 'pending') {
-               throw suspender
+            if (status === 'pending') {
+                throw suspender
             } else if (status === 'error') {
                 throw result
-            } else if ( status === 'success') {
+            } else if (status === 'success') {
                 return result
             }
         }
@@ -28,6 +41,13 @@ function wrapPromise(promise) {
 }
 
 function fetchPosts() {
-    fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
+    return delay(3500)
+        .then(() => fetch('https://jsonplaceholder.typicode.com/posts?_limit=5'))
         .then(res => res.json())
+}
+
+async function fetchUsers() {
+    await delay(1750)
+    const res = await fetch('https://jsonplaceholder.typicode.com/users?_limit=5')
+    return await res.json()
 }
